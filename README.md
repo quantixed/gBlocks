744c9c26cea8588abaf14b30fb54914847501968
Stephen Royle <s.j.royle@warwick.ac.uk>
2019-05-04 21:16:34 +0100

    Added grout
    Removed plain grid generator

--- README.md
+++ README.md
@@ -4,13 +4,19 @@ Generative art projects in R or IGOR pro.
 
 ## R
 
-Take a grid of squares `grid_of_squares.R`
+Generative art based on a grid of squares.
+
+Starting with a grid of squares,
 
 ![img](/img/gridOfSquares.png?raw=true "image")
 
 Apply hysteresis to the corner position in an arc from the first square to the last `grid_of_hysterical_squares.R`
 
 ![img](/img/hystericalSquares.png?raw=true "image")
+
+The grout is variable, as shown in this example.
+
+![img](/img/hystericalSquares2.png?raw=true "image")
 
 
 ## Igor
--- r/grid_of_hysterical squares.R
+++ r/grid_of_hysterical squares.R
@@ -1,18 +1,23 @@
-make_grid_art <- function(xSize,ySize,hFactor) {
+# this function will make grid art
+# arguments define the number of squares in each dimension (xSize, ySize)
+# grout defines the gap between squares (none = 0, max = 1)
+# hFactor defines the amount of hysteresis (none = 0, max = 1, moderate = 10)
+make_grid_art <- function(xSize, ySize, grout, hFactor) {
   xWave <- seq.int(1:xSize)
   yWave <- seq.int(1:ySize)
   axMin <- min(min(xWave) - 1,min(yWave) - 1)
   axMax <- max(max(xWave) + 1,max(yWave) + 1)
-  x <- 0
   nSquares <- length(xWave) * length(yWave)
+  x <- 0
+  halfGrout <- (1 - grout) / 2
   for (i in seq_along(yWave)) {
     yCentre <- yWave[i]
     for (j in seq_along(xWave)) {
-      if(hFactor == 0) {
-        hyst <- rnorm(8, 0.4, 0)
+      if(hFactor < 1) {
+        hyst <- rnorm(8, halfGrout, 0)
       }
       else {
-        hyst <- rnorm(8, 0.4, sin(x / (nSquares - 1) * pi) / hFactor)
+        hyst <- rnorm(8, halfGrout, sin(x / (nSquares - 1) * pi) / hFactor)
       }
       xCentre <- xWave[j]
       lt <- c(xCentre - hyst[1],yCentre - hyst[2])
@@ -41,10 +46,12 @@ make_grid_art <- function(xSize,ySize,hFactor) {
            y1 = multiple_segments[,4])
 }
 # square grid with minimal hysteresis
-make_grid_art(10,10,50)
+make_grid_art(10,10,0.2,50)
 # square grid (more squares) more hysteresis
-make_grid_art(20,20,10)
+make_grid_art(20,20,0.2,10)
 # rectangular grid same hysteresis
-make_grid_art(25,15,10)
+make_grid_art(25,15,0.2,10)
 # same grid with no hysteresis
-make_grid_art(25,15,0)
+make_grid_art(25,15,0.2,0)
+# square grid moderate hysteresis and no grout
+make_grid_art(20,20,0,10)
